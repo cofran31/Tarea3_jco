b0cb054f01eb0c000a960a2104903e29e9d54b1a
Carlos Ortube <cool3000bo@gmail.com>
2018-02-26 01:42:04 -0400

    Se implementa los metodos de la clase pedidoContado

--- PedidoContado.java
+++ PedidoContado.java
@@ -1,5 +1,17 @@
 public class PedidoContado 
 { 
-
+ public PedidoContado(double importe)
+ {
+   super(importe);
+ }
+ 
+ public void paga()
+ {
+ System.out.println("El pago del pedido por importe de:" + importe + " Se ha realizado ");
+ }
+ public booblean valida()
+ {
+ return true;
+ }
 } 
 
